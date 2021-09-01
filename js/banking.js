48198bb67c90f84f6fb4564d2863df36d037454d
Hamzammia <mahmudul.web.developer@gmail.com>
2021-09-01 12:52:54 +0600

    add

--- js/banking.js
+++ js/banking.js
@@ -13,13 +13,15 @@ document.getElementById('deposite-button').addEventListener('click', function ()
 
 
 
-    //upadate toytal balace 
+    //upadate total balace 
 
     const totalbalance = document.getElementById('total-amount');
     const totalbalanceText = totalbalance.innerText;
     const previousTotalBalance = parseFloat(totalbalanceText);
 
     totalbalance.innerText = previousTotalBalance + currentDepositeInput;
+
+    //get withdraw
 
 
 
@@ -28,4 +30,29 @@ document.getElementById('deposite-button').addEventListener('click', function ()
 
     depositeInput.value = '';
 
+})
+
+
+
+document.getElementById('withdraw-button').addEventListener('click', function () {
+    const newWithdraw = document.getElementById('withdraw-input');
+    const newWithdrawText = newWithdraw.value;
+    const newWithdrawAmount = parseFloat(newWithdrawText);
+
+    const totalWithdrawbalance = document.getElementById('withdraw-total');
+    const totalWithdrawbalanceText = totalWithdrawbalance.innerText;
+    const totalWithdrawAmount = parseFloat(totalWithdrawbalanceText);
+
+    totalWithdrawbalance.innerText = totalWithdrawAmount + newWithdrawAmount;
+
+    //update balance total after withdraw
+
+    const balanceTotal = document.getElementById('total-amount');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previousBalanceTotal - newWithdrawAmount;
+    newWithdraw.value = '';
+
+    // console.log(newWithdrawText);
 })
